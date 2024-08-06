66171b06702d25a968a6a56db8689d6c2a94463d
olalekan <lekanmojibola@gmail.com>
2024-08-06 11:23:51 +0100

    basics

--- 0x00-ES6_basic/5-main.js
+++ 0x00-ES6_basic/5-main.js
@@ -1,3 +1,6 @@
-import getBudgetForCurrentYear from './8-getBudgetCurrentYear.js';
+import getFullBudgetObject from './9-getFullBudget.js';
 
-console.log(getBudgetForCurrentYear(400, 700, 900));
+const fullBudget = getFullBudgetObject(20, 50, 10);
+
+console.log(fullBudget.getIncomeInDollars(fullBudget.income));
+console.log(fullBudget.getIncomeInEuros(fullBudget.income));
