ab065a67ce92c7020fe056c8c52940d788f261cc
olalekan <lekanmojibola@gmail.com>
2024-08-07 22:14:02 +0100

    Promise

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,17 @@
+export default function handleResponseFromAPI(promise) {
+  return new Promise((resolve) => {
+    promise
+      .then(() => {
+        resolve({
+          status: 200,
+          body: 'success',
+        });
+      })
+      .catch(() => {
+        resolve(new Error());
+      })
+      .finally(() => {
+        console.log('Got a response from the API');
+      });
+  });
+}
