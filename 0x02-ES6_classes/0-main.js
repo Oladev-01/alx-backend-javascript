bd088074fae712cddb7e4027b02c61c31d1e7380
olalekan <lekanmojibola@gmail.com>
2024-08-09 15:33:43 +0100

    classes

--- 0x02-ES6_classes/0-main.js
+++ 0x02-ES6_classes/0-main.js
@@ -2,3 +2,5 @@ import Currency from "./3-currency.js";
 
 const dollar = new Currency('$', 'Dollars');
 console.log(dollar.displayFullCurrency());
+dollar.name = 'Yen';
+console.log(dollar.displayFullCurrency());
