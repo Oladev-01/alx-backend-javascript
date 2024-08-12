e233f3a04167100834c686928f78bca80ec54710
olalekan <lekanmojibola@gmail.com>
2024-08-12 12:52:58 +0100

    checking

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,7 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+  return Promise.any([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((results) => results.map((results) => {
       if (results.status === 'fulfilled') {
         return {
