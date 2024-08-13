3e68ea2a73a771ba9c6de92885693f83d6698c50
olalekan <lekanmojibola@gmail.com>
2024-08-13 08:54:46 +0100

    check

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,10 @@
 export default function cleanSet(set, string) {
-  if (string.length === 0) { return []; }
   const result = [];
-  for (const str of set) {
-    if (str.startsWith(string)) {
-      result.push(str.slice(string.length));
+  if (string.length !== 0) {
+    for (const str of set) {
+      if (str.startsWith(string)) {
+        result.push(str.slice(string.length));
+      }
     }
   }
   return result.join('-');
