ce4792ed5fdcaf4ea8ee0ad699e00e74cefbcbc4
olalekan <lekanmojibola@gmail.com>
2024-08-14 19:05:53 +0100

    interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -13,6 +13,15 @@ interface Directors extends Teacher {
     numberOfReports: number;
 }
 
+const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+};
+
+console.log(director1);
 // printTeacher function: i need to write the interface for the function first
 interface printTeacherFunction {
     (firstName: string, lastName: string): string;
