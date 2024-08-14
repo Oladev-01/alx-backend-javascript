c64ac00e3c122777f1ade33eceb09ea527eff87f
olalekan <lekanmojibola@gmail.com>
2024-08-14 18:15:10 +0100

    interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -12,3 +12,13 @@ interface Teacher {
 interface Directors extends Teacher {
     numberOfReports: number;
 }
+
+// printTeacher function: i need to write the interface for the function first
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+    return `${firstName.charAt(0)}. ${lastName}`;
+};
+
