aa76c087d6beed63675f194ee2454559a98dacb8
olalekan <lekanmojibola@gmail.com>
2024-08-14 19:00:53 +0100

    interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -22,3 +22,34 @@ const printTeacher: printTeacherFunction = (firstName, lastName) => {
     return `${firstName.charAt(0)}. ${lastName}`;
 };
 
+// console.log(printTeacher("John", "Doe")); testing printTeacher
+
+interface studentClass {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+interface studentClassConstructor {
+    new (firstName: string, lastName: string): studentClass;
+}
+
+class studentClass implements studentClass {
+    firstName: string;
+    lastName: string;
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+    displayName(): string {
+        return `${this.firstName}`;
+    }
+}
+
+// testing for studentClass
+//const newClass = new studentClass("John", "Doe");
+//console.log(newClass);
+//console.log(newClass.workOnHomework());
+//console.log(newClass.displayName());
