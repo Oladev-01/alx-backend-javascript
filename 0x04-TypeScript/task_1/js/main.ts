1fd3dce4bf225307c5804fe329ecc6346004c2cf
olalekan <lekanmojibola@gmail.com>
2024-08-14 15:46:44 +0100

    ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,7 +1,14 @@
 interface Teacher {
     readonly firstName: string;
     fullTimeEmployee: boolean;
-    readonly lastName: string;
+    yearsOfExperience?: string;
+    readonly lastName: number;
     location: string;
     [key: string]: any;
+}
+
+// extending the Teacher interface
+
+interface Directors extends Teacher {
+    numberOfReports: number;
 }
