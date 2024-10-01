76121f6b3854c3d2fd47a487f261c174cd38c523
olalekan <lekanmojibola@gmail.com>
2024-10-01 14:16:38 +0100

    nodejs

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,8 +2,14 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (inp) => {
   const name = inp.toString().trim();
   console.log(`Your name is: ${name}`);
+  process.exit();
 });
 
 process.on('exit', () => {
   console.log('This important software is now closing');
+});
+
+process.on('SIGINT', () => {
+  console.log('\nThis important software is now closing');
+  process.exit();
 });
