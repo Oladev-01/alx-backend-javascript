ec8d4c5543d03f1604ea2f89e1cde34b727df961
olalekan <lekanmojibola@gmail.com>
2024-10-02 17:49:25 +0100

    app

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -5,7 +5,6 @@ function countStudents(path) {
   return new Promise((res, rej) => {
     fs.readFile(path, 'utf8', (err, data) => {
       if (err) {
-        // eslint-disable-next-line prefer-promise-reject-errors
         rej('Cannot load data from database');
       } else {
         const dataLines = data.split('\n').filter((line) => line.trim() !== '');
@@ -19,7 +18,7 @@ function countStudents(path) {
           studentData.forEach((std) => {
             const student = std.split(',');
             if (student.length !== headers.length) {
-              rej();
+              rej('Invalid student data format');
             }
             const field = student[student.length - 1];
             if (!fieldCounts[field]) {
@@ -38,23 +37,27 @@ function countStudents(path) {
     });
   });
 }
+
 const app = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
   if (req.url === '/') {
     res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
+    res.setHeader('Content-Type', 'text/plain');
     const path = process.argv[2];
     if (!path) {
       res.statusCode = 500;
+      res.end('Path to the database is missing');
     } else {
       countStudents(path)
         .then((data) => {
           res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/plain');
           res.end(data);
         }).catch((error) => {
           res.statusCode = 404;
-          res.end(error);
+          res.end(String(error));
         });
     }
   }
