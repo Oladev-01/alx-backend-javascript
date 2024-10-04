87ace8d34039f007aae492aaae13da8efa973ff8
olalekan <lekanmojibola@gmail.com>
2024-10-04 16:31:37 +0100

    test

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,23 @@
+const { expect } = require('chai');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', () => {
+    it('should return the expected value for SUM', () => {
+        expect(calculateNumber('SUM', 2, 4.5)).to.equal(7),
+        expect(calculateNumber('SUM', 2, -4.5)).to.equal(-2),
+        expect(calculateNumber('SUM', -2, -4.5)).to.equal(-6)
+    });
+
+    it('should return expected value for SUBTRACT', () => {
+        expect(calculateNumber('SUBTRACT', -2, -4.5)).to.equal(2),
+        expect(calculateNumber('SUBTRACT', 2, -4.5)).to.equal(6)
+    });
+
+    it('should return the expected value for DIVIDE', () => {
+        expect(calculateNumber('DIVIDE', -2, 4.5)).to.equal(-0.4),
+        expect(calculateNumber('DIVIDE', 2, 0)).to.equal('Error'),
+        expect(calculateNumber('DIVIDE', 0, 4.5)).to.equal(0),
+        expect(calculateNumber('DIVIDE', -0, -0)).to.equal('Error'),
+        expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2)
+    });
+});
