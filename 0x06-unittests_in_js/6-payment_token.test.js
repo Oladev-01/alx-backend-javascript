6a669077a125e3b58855a69aee99ac583bb1debc
olalekan <lekanmojibola@gmail.com>
2024-10-06 17:44:15 +0100

    test

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -2,15 +2,16 @@ const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
-    it('should return a resolved promise', () => {
+    it('should return a resolved promise', (done) => {
         getPaymentTokenFromAPI(true)
             .then((response) => {
                 expect(response).to.include({ data: 'Successful response from the API' });
                 done();
             }).catch((error) => done(error));
     });
-    it('should do nothing for this test', () => {
-       const response = getPaymentTokenFromAPI(false);
-       expect(response).to.be.undefined;
+    it('should do nothing for this test', (done) => {
+       const response = getPaymentTokenFromAPI(false)
+        expect(response).to.be.undefined;
+        done();
     });
 });
